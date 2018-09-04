bfc46090a3ac082720f3a9f788ddc2748f6650ee
breizzh <remisamsoninvestisseur@gmail.com>
2018-09-04 13:28:23 +0200

    [View_/users/edit] Editing of the User model to solve the access problem to the page

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,5 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
 end
