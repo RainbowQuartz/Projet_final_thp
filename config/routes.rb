aae26cb62562ed7e91890a21e8ce232f18df97b0
Léona Chevrel <chevrel.leona@gmail.com>
2018-09-05 14:16:11 +0200

    changed the user controllers from the defaault devise to users/ ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    confirmations: 'users/confirmations',
+                                    passwords: 'users/passwords',
+                                    unlocks: 'users/unlocks'}
 	resources :users
 
   root to: 'static_pages#home'
