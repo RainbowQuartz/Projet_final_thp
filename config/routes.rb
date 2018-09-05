721c37b06e4605edd83be92be8e0f6e7b074b3b1
Léona Chevrel <chevrel.leona@gmail.com>
2018-09-05 14:19:11 +0200

    forgot these ones

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     confirmations: 'users/confirmations',
                                     passwords: 'users/passwords',
-                                    unlocks: 'users/unlocks'}
+                                    unlocks: 'users/unlocks',
+                                    sessions: 'users/sessions'}
 	resources :users
 
   root to: 'static_pages#home'
