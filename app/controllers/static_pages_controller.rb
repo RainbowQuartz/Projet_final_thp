fc964f95d7d2299fcb5cdfbb0567ba24ef565521
Léona Chevrel <chevrel.leona@gmail.com>
2018-09-11 11:00:44 +0200

    sorted the users suggestion hash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,12 +13,16 @@ class StaticPagesController < ApplicationController
   private
 
   def suggestions
-    @comparisons_hash = {}
-    User.all.each do |compared_user|
-      points = SuggestionAlgorithm.perform(current_user, compared_user)
-      p '-' *20 + points.to_s + '-' * 20
-      @comparisons_hash[compared_user.id] = points
+    if user_signed_in?
+      @comparisons_hash = {}
+      User.all.each do |compared_user|
+        points = SuggestionAlgorithm.perform(current_user, compared_user)
+        p '-' *20 + points.to_s + '-' * 20
+        @comparisons_hash[compared_user.id] = points
+      end
+      p @comparisons_hash
+      @comparisons_hash.sort_by { |_key, value| value }.to_h
+      p @comparisons_hash
     end
-    p @comparisons_hash
   end
 end
