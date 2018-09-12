5dba7831c9b8ed7eef49bf4c06f04418c8810095
Romain <romainsaillour@orange.fr>
2018-09-12 11:49:25 +0200

    fix conflict

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,14 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :chat
   belongs_to :user
-  has_many :notifications, as: :event
-
-  after_create :send_notification
-
-  private
-    def send_notification(message)
-      message.notifications.create(user: message.recipient)
-    end
-  end
 
 end
